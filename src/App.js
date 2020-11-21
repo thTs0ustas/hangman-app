a4c87d3e8865029ff2cf418dfb1b63ce84e2ee87
thTs0ustas <60882323+thTs0ustas@users.noreply.github.com>
2020-11-21 23:49:44 +0200

    random words are functional again

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import React from "react";
 import "./App.css";
-import {Game} from "./components/game/Game";
-import {Grid} from "@material-ui/core";
+import { Game } from "./components/game/Game";
+import { Grid } from "@material-ui/core";
 
-const aBetterWay = (word, guessed) =>
-   word.split('').map(e => guessed.includes(e) ? `${e} ` : '_ ').join('')
+import randomWords from "random-words";
 
+// const aBetterWay = (word, guessed) =>
+//    word.split('').map(e => guessed.includes(e) ? `${e} ` : '_ ').join('')
 
-console.log(aBetterWay('black', 'acdfrt'))
-
+// console.log(randomWords());
 
 function App() {
-   return (
-      <Grid container alignContent={"center"}>
-         <Grid xs={12} sm={10} item>
-            <Game/>
-         </Grid>
+  return (
+    <Grid container alignContent={"center"}>
+      <Grid xs={12} sm={10} item>
+        <Game />
       </Grid>
-   );
+    </Grid>
+  );
 }
 
 export default App;
--- src/epics/epics.js
+++ src/epics/epics.js
@@ -1,39 +1,50 @@
-import {map, mapTo, switchMap, tap} from "rxjs/operators";
+import { map, mapTo, tap } from "rxjs/operators";
+import randomWords from "random-words";
+// import { switchMap } from "rxjs/operators";
+// import { ajax } from "rxjs/ajax";
 
-import {ajax} from "rxjs/ajax";
 import {
-   correctTry,
-   letsStart,
-   settingGaps,
-   settingNewGaps,
-   settingWord,
-   winnerOrLoser,
-   wrongTry,
+  correctTry,
+  letsStart,
+  settingGaps,
+  settingNewGaps,
+  settingWord,
+  winnerOrLoser,
+  wrongTry,
 } from "../redux/actions";
-import {combineEpics, ofType} from "redux-observable";
+
+import { combineEpics, ofType } from "redux-observable";
+
+// const epicWords = (action$) =>
+//   action$.pipe(
+//     ofType("LETS_START"),
+//     switchMap(() =>
+//       ajax
+//         .getJSON("https://random-word-api.herokuapp.com/word?number=10")
+//         .pipe(
+//           tap(words => console.log('Words_response',words)),
+//           map((response) =>
+//             settingWord(response[Math.floor(Math.random() * 10)])
+//           ),
+//           tap(word => console.log('Word_response',word)),
+//         )
+//     )
+//   );
 
 const epicWords = (action$) =>
   action$.pipe(
     ofType("LETS_START"),
-    switchMap(() =>
-      ajax
-        .getJSON("https://random-word-api.herokuapp.com/word?number=10")
-        .pipe(
-          tap(words => console.log('Words_response',words)),
-          map((response) =>
-            settingWord(response[Math.floor(Math.random() * 10)])
-          ),
-          tap(word => console.log('Word_response',word)),
-        )
-    )
+    map(() => randomWords()),
+    tap((word) => console.log(word)),
+    map((word) => settingWord(word))
   );
 
 const epicTries = (action$) =>
   action$.pipe(
     ofType("SETTING_WORD"),
     map((action) => {
-       const gapsArray = Array(action.payload.length).fill("_ ");
-       return settingGaps(gapsArray);
+      const gapsArray = Array(action.payload.length).fill("_ ");
+      return settingGaps(gapsArray);
     })
   );
 
@@ -41,10 +52,10 @@ const epicLetters = (action$, state$) =>
   action$.pipe(
     ofType("SETTING_LETTER"),
     map((action) => {
-       if (!state$.value.word.includes(action.payload)) {
-          return wrongTry();
-       }
-       return correctTry(action.payload);
+      if (!state$.value.word.includes(action.payload)) {
+        return wrongTry();
+      }
+      return correctTry(action.payload);
     })
   );
 
@@ -52,24 +63,28 @@ const epicCorrectness = (action$, state$) =>
   action$.pipe(
     ofType("CORRECT_LETTER"),
     map(() =>
-       settingNewGaps(state$.value.word.split('')
-          .map(e=> state$.value.guess.includes(e)? `${e} ` : '_ ')
-          .join('')))
+      settingNewGaps(
+        state$.value.word
+          .split("")
+          .map((e) => (state$.value.guess.includes(e) ? `${e} ` : "_ "))
+          .join("")
+      )
+    )
   );
 
 const epicWinningOrLosing = (action$, state$) =>
   action$.pipe(
     ofType("SETTING_WINNER_LOSER"),
     map(() => {
-       const {gaps, word, tries, status, wrongTries} = state$.value;
-       let newStatus = status
-       if (word === undefined || tries === 0) return newStatus
-       if (!gaps.includes("_")) newStatus = "We have a Winner !!!"
-       if (wrongTries === 6) newStatus = "Aaand I'm dead !!!"
+      const { gaps, word, tries, status, wrongTries } = state$.value;
+      let newStatus = status;
+      if (word === undefined || tries === 0) return newStatus;
+      if (!gaps.includes("_")) newStatus = "We have a Winner !!!";
+      if (wrongTries === 6) newStatus = "Aaand I'm dead !!!";
 
-       return newStatus
+      return newStatus;
     }),
-    map(new_status => winnerOrLoser(new_status))
+    map((new_status) => winnerOrLoser(new_status))
   );
 
 const epicRestart = (action$) =>
